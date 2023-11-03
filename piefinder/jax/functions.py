a0b8e234cac51915743a95ac5848669aad9152b2
Ted Johnson <ted.johnson@unlv.edu>
2023-11-03 14:39:26 -0700

    added some more interesting functions

--- piefinder/jax/functions.py
+++ piefinder/jax/functions.py
@@ -60,3 +60,92 @@ class Constant(BaseFunction):
         def func(_t):
             return k
         super().__init__(func)
+
+class Fourier(BaseFunction):
+    """
+    A Fourier function.
+    
+    Parameters
+    ----------
+    lam : float
+        The longest wavelength to consider.
+    *args
+        The Fourier coefficients.
+    """
+    def __init__(self, lam: float, *args: float):
+        def func(t):
+            sum = jnp.zeros_like(t)
+            for i, arg in enumerate(args):
+                n = i // 2 + 1  # Compute the Fourier series index (n)
+                if i % 2 == 0:
+                    sum += arg * jnp.cos(2 * jnp.pi * n * t / lam)  # Cosine term
+                else:
+                    sum += arg * jnp.sin(2 * jnp.pi * n * t / lam)  # Sine term
+            return sum
+        super().__init__(func)
+
+class Polynomial(BaseFunction):
+    """
+    A polynomial function.
+    
+    Parameters
+    ----------
+    *args
+        The polynomial coefficients.
+    """
+    def __init__(self, *args: float):
+        def func(t):
+            sum = jnp.zeros_like(t)
+            for i, arg in enumerate(args):
+                sum += arg * jnp.power(t, i)
+            return sum
+        super().__init__(func)
+
+class Flare(BaseFunction):
+    """
+    A flare function.
+    
+    Parameters
+    ----------
+    tpeak : float
+        The peak time of the flare.
+    fwhm : float
+        The full width at half maximum of the flare.
+    amp : float
+        The amplitude of the flare.
+    
+    Notes
+    -----
+    This is a reimplementation of code written
+    by Tom Barclay, https://github.com/mrtommyb/xoflares
+    """
+    def __init__(
+        self,
+        tpeak: float,
+        fwhm: float,
+        amp: float
+    ):
+        def func(t):
+            t_half = (t - tpeak) / fwhm
+            coeffs = [
+                1.0,
+                1.941,
+                -0.175,
+                -2.246,
+                -1.125
+            ]
+            f_rise = lax.bitwise_and(t_half >= -1, t_half <= 0) * (
+                coeffs[0] + coeffs[1] * t_half + coeffs[2] * jnp.power(t_half, 2) + coeffs[3] * jnp.power(t_half, 3) + coeffs[4] * jnp.power(t_half, 4)
+            )
+            coeffs = [
+                0.6890,
+                -1.600,
+                0.3030,
+                -0.2783,
+            ]
+            f_decay = lax.bitwise_and(t_half > 0, t_half <= 20) * (
+                coeffs[0] * jnp.exp(coeffs[1] * t_half) + coeffs[2] * jnp.exp(coeffs[3] * t_half)
+            )
+
+            return (f_rise + f_decay) * amp
+        super().__init__(func)
--- test/test_jax_functions.py
+++ test/test_jax_functions.py
@@ -1,6 +1,7 @@
 """
 Tests for piefinder.jax.functions
 """
+from jax import numpy as jnp
 
 from piefinder.jax.functions import BaseFunction, add, multiply
 
@@ -46,3 +47,60 @@ def test_constant():
     k = 2
     func = functions.Constant(k)
     assert func(1) == 2
+
+def test_fourier():
+    """
+    Test the Fourier function
+    """
+    lam = 2
+    args = [1, 2]
+    # = 1*cos(2*pi*0*t/lam)
+    # + 2*sin(2*pi*1*t/lam)
+    func = functions.Fourier(lam, *args)
+    t = 0
+    expected = 1
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+    t = 0.5
+    expected = 2
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+
+def test_polynomial():
+    """
+    Test the Polynomial function
+    """
+    args = [1, 2]
+    # = 1 + 2*t
+    func = functions.Polynomial(*args)
+    t = 0
+    expected = 1
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+    t = 0.5
+    expected = 2
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+
+def test_flare():
+    """
+    Test the Flare function
+    """
+    tpeak = 0
+    fwhm = 1
+    amp = 2
+    func = functions.Flare(tpeak, fwhm, amp)
+    t = 0
+    expected = 2
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+    t = 100
+    expected = 0
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+    t = -100
+    expected = 0
+    assert func(t) == expected, f'got {func(t)}, expected {expected}'
+    times = jnp.linspace(-10, 10, 100)
+    lc = func(times)
+    assert lc.shape == (100,)
+    before = times<0
+    after = times>0
+    assert jnp.all(jnp.diff(lc[before]) >= 0)
+    assert jnp.all(jnp.diff(lc[after]) <= 0)
+    assert ~jnp.any(lc>amp), 'Flare should not be above amplitude'
+    assert ~jnp.any(lc<0), 'Flare should not be negative'
