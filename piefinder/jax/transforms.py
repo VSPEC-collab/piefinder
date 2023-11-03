a4fb34436dfda220caa7329930b86f62360352a4
Ted Johnson <ted.johnson@unlv.edu>
2023-11-03 15:35:32 -0700

    add some more interesting transforms

--- piefinder/jax/transforms.py
+++ piefinder/jax/transforms.py
@@ -5,6 +5,8 @@ gradient descent more easily.
 
 from typing import Callable
 
+from jax import jit, numpy as jnp
+
 class BaseTransform:
     """
     Base class for transforms.
@@ -21,8 +23,8 @@ class BaseTransform:
         func: Callable,
         inv_func: Callable
     ):
-        self._func = func
-        self._inv_func = inv_func
+        self._func = jit(func)
+        self._inv_func = jit(inv_func)
     def __call__(self, x):
         """
         Transform the input.
@@ -53,3 +55,52 @@ class BaseTransform:
             The original input.
         """
         return self._inv_func(y)
+
+class Identity(BaseTransform):
+    """
+    The identity transform.
+    """
+    def __init__(self):
+        def func(x):
+            return x
+        def inv(x):
+            return x
+        super().__init__(func, inv)
+
+class Log10(BaseTransform):
+    """
+    The log10 transform.
+    """
+    def __init__(self):
+        def func(x):
+            return jnp.log10(x)
+        def inv(x):
+            return jnp.power(10, x)
+        super().__init__(func, inv)
+
+class Sigmoid(BaseTransform):
+    """
+    The sigmoid transform.
+    """
+    def __init__(self):
+        def func(x):
+            return 1 / (1 + jnp.exp(-x))
+        def inv(x):
+            return jnp.log(x / (1 - x))
+        super().__init__(func, inv)
+
+class Tanh(BaseTransform):
+    """
+    The tanh transform.
+    
+    .. warning::
+        This transform is not invertable for large values.
+        This is because the floating point precision cannot
+        differentiate between tanh(10) and 1.
+    """
+    def __init__(self):
+        def func(x):
+            return jnp.tanh(x)
+        def inv(x):
+            return jnp.arctanh(x)
+        super().__init__(func, inv)
--- test/test_jax_transforms.py
+++ test/test_jax_transforms.py
@@ -1,15 +1,23 @@
 """
 Tests for piefinder.jax.transforms
 """
-from jax import numpy as jnp
+from jax import numpy as jnp, config
+import pytest
 
 from piefinder.jax import transforms
+
+config.update("jax_enable_x64", True)
+
 
 def inverse_testing(tr:transforms.BaseTransform, x):
     """
     Generalized test to test the validity of the inverse transform.
     """
-    assert jnp.all(tr.inv(tr(x)) == x), f'failed for x = {x}'
+    try:
+        assert jnp.all(jnp.isclose(tr.inv(tr(x)), x, rtol=1e-4)), f'failed for x = {x}'
+    except AssertionError:
+        for elem in x:
+            assert jnp.isclose(tr.inv(tr(elem)), elem, rtol=1e-4), f'failed for x = {elem}, {(tr.inv(tr(elem))-elem)/elem*100} % error'
 
 def test_base_transform():
     """
@@ -27,4 +35,44 @@ def test_base_transform():
     x = jnp.array([1, 2, 3])
     inverse_testing(func, x)
     
+def test_identity():
+    """
+    Test Identity
+    """
+    func = transforms.Identity()
+    assert func(1) == 1
+    assert func.inv(1) == 1
+    x = jnp.array([1, 2, 3])
+    inverse_testing(func, x)
+
+def test_log10():
+    """
+    Test Log10
+    """
+    func = transforms.Log10()
+    assert func(10) == pytest.approx(1, rel=1e-6)
+    assert func.inv(1) == pytest.approx(10, rel=1e-6)
+    x = jnp.array([0.1 ,1, 10, 100])
+    inverse_testing(func, x)
     
+def test_sigmoid():
+    """
+    Test Sigmoid
+    """
+    func = transforms.Sigmoid()
+    assert func(0) == pytest.approx(0.5, rel=1e-6)
+    assert func.inv(0.5) == pytest.approx(0, rel=1e-6)
+    x = jnp.array([-10, -1, 0, 1, 10])
+    inverse_testing(func, x)
+
+def test_tanh():
+    """
+    Test Tanh
+    
+    """
+    func = transforms.Tanh()
+    assert func(0) == pytest.approx(0, rel=1e-6)
+    assert func(4) < 1.0
+    assert func.inv(0) == pytest.approx(0, rel=1e-6)
+    x = jnp.array([-10, -1, 0, 1, 10])
+    inverse_testing(func, x)
